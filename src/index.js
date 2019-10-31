05a7be01d39052bbe586e8cb741b9ba5e888bc96
Sebastian Landwehr <info@dword-design.de>
2019-10-31 18:24:42 +0100

    add node-sass-import

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import { base } from '@dword-design/base'
 import { spawn } from 'child_process'
 
 export default () => base({
-  prepare: () => spawn('node-sass', ['src', '--output', 'dist'], { stdio: 'inherit' }),
-  start: () => spawn('node-sass', ['--watch', 'src', '--output', 'dist'], { stdio: 'inherit' }),
+  prepare: () => spawn('node-sass', ['src', '--output', 'dist', '--importer', require.resolve('node-sass-import')], { stdio: 'inherit' }),
+  start: () => spawn('node-sass', ['--watch', 'src', '--output', 'dist', '--importer', require.resolve('node-sass-import')], { stdio: 'inherit' }),
 })
