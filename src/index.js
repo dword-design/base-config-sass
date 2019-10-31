4e9157d34700879216674666a5c1f1ee8fb74314
dword <sebastianlandw@gmail.com>
2019-10-31 17:17:06 +0000

    bugfixes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { base } from '@dword-design/base'
 import { spawn } from 'child_process'
 
-export default base({
-  prepare: () => spawn('node-sass', ['src', 'dist'], { stdio: 'inherit' }),
-  start: () => spawn('node-sass', ['--watch', 'src', 'dist'], { stdio: 'inherit' }),
+export default () => base({
+  prepare: () => spawn('node-sass', ['src', '--output', 'dist'], { stdio: 'inherit' }),
+  start: () => spawn('node-sass', ['--watch', 'src', '--output', 'dist'], { stdio: 'inherit' }),
 })
