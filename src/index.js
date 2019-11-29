cc5c021e9a655c35a0e2c8280b05d0f9efa92451
dword <sebastianlandw@gmail.com>
2019-11-29 09:26:18 +0000

    copy other files as well

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import { base } from '@dword-design/base'
 import { spawn } from 'child_process'
 import resolveBin from 'resolve-bin'
-import glob from 'glob-promise'
-import { map, promiseAll } from '@functions'
-import { copyFile } from 'fs'
-import P from 'path'
+import { copy, remove } from 'fs'
 
 export default () => base({
   prepare: async () => {
+    await remove('dist')
+    await copy('src', 'dist')
     await spawn(resolveBin.sync('node-sass'), ['src', '--output', 'dist', '--importer', require.resolve('node-sass-import')], { stdio: 'inherit' })
-    const sassFilenames = await glob('**/*.{scss,sass}', { cwd: 'src' })
-    await sassFilenames |> map(filename => copyFile(P.resolve('src', filename), P.join('dist', filename))) |> promiseAll
   },
   start: () => spawn(resolveBin.sync('node-sass'), ['--watch', 'src', '--output', 'dist', '--importer', require.resolve('node-sass-import')], { stdio: 'inherit' }),
 })
--- test/prepare.test.js
+++ test/prepare.test.js
@@ -5,18 +5,20 @@ import outputFiles from 'output-files'
 import resolveBin from 'resolve-bin'
 import { minimalProjectConfig } from '@dword-design/base'
 import glob from 'glob-promise'
-import { endent } from '@functions'
+import { endent, omit } from '@functions'
+import { readFile } from 'fs'
+import P from 'path'
 
 export const it = () => withLocalTmpDir(__dirname, async () => {
   await outputFiles({
-    ...minimalProjectConfig,
-    'dist/test.txt': 'foo',
+    ...minimalProjectConfig |> omit('src/index.js'),
+    'dist/foo.txt': 'foo',
     'src/foo/test.sass': endent`
       $color: blue
       body
         color: $color
     `,
-    'src/foo.txt': 'foo',
+    'src/test.txt': 'foo',
     'src/index.scss': endent`
       $color: red;
       body {
@@ -33,6 +35,14 @@ export const it = () => withLocalTmpDir(__dirname, async () => {
     'index.scss',
     'test.txt',
   ])
+  expect(await readFile(P.resolve('dist', 'foo', 'test.css'), 'utf8')).toEqual(endent`
+    body {
+      color: blue; }
+  ` + '\n')
+  expect(await readFile(P.resolve('dist', 'index.css'), 'utf8')).toEqual(endent`
+    body {
+      background: red; }
+  ` + '\n')
   expect(stdout).toMatch(/^Copying config files …\nUpdating README.md …\nRendering Complete, saving \.css file\.\.\.\nWrote CSS to .*?\/dist\/foo\/test\.css\nRendering Complete, saving \.css file\.\.\.\nWrote CSS to .*?\/dist\/index\.css\nWrote 2 CSS files to .*?\/dist\n$/)
 })
 export const timeout = 10000
