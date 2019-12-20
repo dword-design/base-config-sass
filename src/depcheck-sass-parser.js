3c9aa787c2886c08ca971f2200725ef7e9684bd1
Sebastian Landwehr <info@dword-design.de>
2019-12-20 04:55:13 +0100

    fix depcheck for css files

--- src/depcheck-sass-parser.js
+++ src/depcheck-sass-parser.js
@@ -1,10 +1,17 @@
 import getPackageName from 'get-package-name'
+import requirePackageName from 'require-package-name'
 import importer from './importer'
 import sass from 'node-sass'
-import { map, uniq, unary, compact } from '@dword-design/functions'
+import { map, uniq, compact } from '@dword-design/functions'
 
 export default (content, filePath) => {
   const { stats } = sass.renderSync({ file: filePath, importer })
 
-  return stats.includedFiles |> map(unary(getPackageName)) |> compact |> uniq
+  return stats.includedFiles
+    |> map(path => path[0] === '~'
+      ? requirePackageName(path.substr(1))
+      : getPackageName(path)
+    )
+    |> compact
+    |> uniq
 }
