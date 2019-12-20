f54421984fcffb0dcc20e3256b744f4361edb406
Sebastian Landwehr <info@dword-design.de>
2019-12-20 04:04:54 +0100

    check file extensions when importing js

--- src/js-importer.js
+++ src/js-importer.js
@@ -5,7 +5,7 @@ import P from 'path'
 export default (url, prev) => {
   if (url[0] === '~') {
     const path = resolveFrom.silent(P.dirname(prev), url.substr(1))
-    if (path !== undefined) {
+    if (path !== undefined && ['.js', '.json'].includes(P.extname(path))) {
       return { contents: jsontosass.convert(JSON.stringify(require(path))) }
     }
   }
---
+++ test/build/contrib-main-scss-import.test.js
@@ -0,0 +1,40 @@
+import { spawn } from 'child-process-promise'
+import withLocalTmpDir from 'with-local-tmp-dir'
+import outputFiles from 'output-files'
+import packageConfig from '../package.config'
+import sortPackageJson from 'sort-package-json'
+import expect from 'expect'
+import { readFile } from 'fs-extra'
+import P from 'path'
+import { endent } from '@dword-design/functions'
+
+export default () => withLocalTmpDir(__dirname, async () => {
+  await outputFiles({
+    'node_modules/bar': {
+      'index.js': 'module.exports = 1',
+      'index.scss': '$color: red;',
+      'package.json': JSON.stringify({ main: 'index.scss' }),
+    },
+    'package.json': JSON.stringify(sortPackageJson({
+      ...packageConfig,
+      dependencies: {
+        'bar': '^1.0.0',
+      },
+      devDependencies: {
+        '@dword-design/base-config-sass': '^1.0.0',
+      },
+    }), undefined, 2),
+    'src/index.scss': endent`
+      @import '~bar';
+
+      body {
+        background: $color;
+      }
+    `,
+  })
+  await spawn('base', ['build'])
+  expect(await readFile(P.resolve('dist', 'index.css'), 'utf8')).toEqual(endent`
+    body {
+      background: red; }
+  ` + '\n')
+})
