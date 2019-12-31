8946b573ad202d73e086aedc6dd518a9111976cf
dword <sebastianlandw@gmail.com>
2019-12-31 03:52:25 +0000

    generate files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import { remove, copy } from 'fs-extra'
-import chokidar from 'chokidar'
-import debounce from 'debounce'
 import nodeConfig from '@dword-design/base-config-node'
 import { merge } from '@dword-design/functions'
 import depcheckSassParser from '@dword-design/depcheck-sass-parser'
@@ -14,25 +12,11 @@ const build = async () => {
 
 export default {
   build,
-  start: () => chokidar
-    .watch('src')
-    .on(
-      'all',
-      debounce(
-        async () => {
-          try {
-            await build()
-          } catch (error) {
-            console.log(error)
-          }
-        },
-        200
-      )
-    ),
   depcheckConfig: nodeConfig.depcheckConfig
     |> merge({
       parsers: {
         '*.scss': depcheckSassParser,
       },
     }),
+  main: 'index.scss',
 }
