ac52f6a601d634fef8f6d5bc2d751bd8c6e1f9ed
Sebastian Landwehr <sebastianlandw@gmail.com>
2020-05-28 21:16:45 +0200

    fix: upgrades

--- src/prepublish-only.spec.js
+++ src/prepublish-only.spec.js
@@ -7,53 +7,56 @@ import { readFile } from 'fs-extra'
 import P from 'path'
 
 export default {
-  valid: () => withLocalTmpDir(async () => {
-    await outputFiles({
-      'dist/foo.txt': 'foo',
-      'package.json': endent`
-        {
-          "baseConfig": "sass",
-          "devDependencies": {
-            "@dword-design/base-config-sass": "^1.0.0"
-          }
-        }
-      `,
-      src: {
-        'foo/test.scss': endent`
+  valid: () =>
+    withLocalTmpDir(async () => {
+      await outputFiles({
+        'dist/foo.txt': 'foo',
+        'package.json': JSON.stringify(
+          {
+            baseConfig: require.resolve('.'),
+          },
+          undefined,
+          2
+        ),
+        src: {
+          'foo/test.scss': endent`
           $color: blue
           body
             color: $color
         `,
-        'test.txt': 'foo',
-        'index.scss': endent`
+          'test.txt': 'foo',
+          'index.scss': endent`
           $color: red;
           body {
             background: $color;
           }
         `,
-      },
-    })
-    const { all } = await execa.command('base prepublishOnly', { all: true })
-    expect(await glob('**', { cwd: 'dist', dot: true })).toEqual([
-      'foo',
-      'foo/test.scss',
-      'index.scss',
-      'test.txt',
-    ])
-    expect(await readFile(P.resolve('dist', 'foo', 'test.scss'), 'utf8')).toEqual(endent`
+        },
+      })
+      await execa.command('base prepare')
+      const { all } = await execa.command('base prepublishOnly', { all: true })
+      expect(await glob('**', { cwd: 'dist', dot: true })).toEqual([
+        'foo',
+        'foo/test.scss',
+        'index.scss',
+        'test.txt',
+      ])
+      expect(await readFile(P.resolve('dist', 'foo', 'test.scss'), 'utf8'))
+        .toEqual(endent`
       $color: blue
       body
         color: $color
     `)
-    expect(await readFile(P.resolve('dist', 'index.scss'), 'utf8')).toEqual(endent`
+      expect(await readFile(P.resolve('dist', 'index.scss'), 'utf8'))
+        .toEqual(endent`
       $color: red;
       body {
         background: $color;
       }
     `)
-    expect(all).toEqual(endent`
+      expect(all).toEqual(endent`
       Copying sass files …
       Sass files successfully copied.
     `)
-  }),
+    }),
 }
