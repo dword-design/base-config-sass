228a0e9697c588abdc232c8411362bec9cae7899
dword <info@dword-design.de>
2021-03-19 18:04:27 +0000

    fix: do not copy test files

--- src/prepublish-only.js
+++ src/prepublish-only.js
@@ -1,8 +1,15 @@
+import { map } from '@dword-design/functions'
 import { copy, remove } from 'fs-extra'
+import globby from 'globby'
 
 export default async () => {
   console.log('Copying sass files …')
   await remove('dist')
   await copy('src', 'dist')
+  await Promise.all(
+    globby('**/*.spec.js', { absolute: true, cwd: 'dist' })
+      |> await
+      |> map(path => remove(path))
+  )
   console.log('Sass files successfully copied.')
 }
