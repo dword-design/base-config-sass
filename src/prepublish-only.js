fc6b93db96cedd83cb946b9e0a9fe29d6745e26e
Sebastian Landwehr <info@sebastianlandwehr.com>
2023-04-09 15:54:14 +0000

    fix: foo

--- src/prepublish-only.js
+++ src/prepublish-only.js
@@ -1,15 +1,15 @@
 import { map } from '@dword-design/functions'
-import { copy, remove } from 'fs-extra'
-import globby from 'globby'
+import fs from 'fs-extra'
+import { globby } from 'globby'
 
 export default async () => {
   console.log('Copying sass files …')
-  await remove('dist')
-  await copy('src', 'dist')
+  await fs.remove('dist')
+  await fs.copy('src', 'dist')
   await Promise.all(
     globby('**/*.spec.js', { absolute: true, cwd: 'dist' })
       |> await
-      |> map(path => remove(path))
+      |> map(path => fs.remove(path)),
   )
   console.log('Sass files successfully copied.')
 }
